1b073f36e96508fad65ad00ce20763b68bdd784e
Flipps12 <paginasypruebas12@gmail.com>
2024-09-18 22:41:45 -0300

    Status

--- api-gateway/src/main/java/com/ronnie/api_gateway/controllers/ApiGatewayController.java
+++ api-gateway/src/main/java/com/ronnie/api_gateway/controllers/ApiGatewayController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/server")
@@ -22,7 +23,7 @@ public class ApiGatewayController {
     }
 
     @GetMapping()
-    public StatusResponse getAllStatus() {
+    public Mono<StatusResponse> getAllStatus() {
         return apiGatewayService.getAllStatus();
     }
 }
--- api-gateway/src/main/java/com/ronnie/api_gateway/services/ApiGatewayService.java
+++ api-gateway/src/main/java/com/ronnie/api_gateway/services/ApiGatewayService.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
@@ -13,23 +14,31 @@ public class ApiGatewayService {
 
     private final WebClient.Builder webClientBuilder;
 
-    public StatusResponse getAllStatus() {
-        return StatusResponse.builder()
-                .gateway("online")
-                .auth(getStatus("auth"))
-                .notification(getStatus("email"))
-                .mercado_pago(getStatus("mercado-pago"))
-                .order(getStatus("order"))
-                .product(getStatus("product"))
-                .build();
+    public Mono<StatusResponse> getAllStatus() {
+        return Mono.zip(
+                Mono.just("online"),
+                getStatus("auth", "8084"),
+                getStatus("email", "8085"),
+                getStatus("mercado-pago", "8083"),
+                getStatus("order", "8081"),
+                getStatus("product", "8082")
+        ).map(tuple -> StatusResponse.builder()
+                .gateway(tuple.getT1())
+                .auth(tuple.getT2())
+                .notification(tuple.getT3())
+                .mercado_pago(tuple.getT4())
+                .order(tuple.getT5())
+                .product(tuple.getT6())
+                .build());
     }
 
-    public String getStatus(String api) {
-        return webClientBuilder.build() // mejorar peticion, etc
+    public Mono<String> getStatus(String api, String port) {
+        return webClientBuilder.build()
                 .get()
-                .uri("http://localhost:8080/api/" + api + "/status")
+                .uri("http://localhost:" + port + "/api/" + api + "/status")
                 .retrieve()
                 .bodyToMono(String.class)
-                .block();
+                .onErrorResume(e -> Mono.just("offline"));
     }
+
 }
